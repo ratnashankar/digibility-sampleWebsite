4b96092025a615e8e93c8e4c23fcab1c1ccb4931
ratnashankar <ratan.shankar@gmail.com>
2025-08-17 18:13:54 +0530

    corrected image name

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Loading from "./components/Loading";
-import pho5 from "../src/assets/storybg.JPG";
+import pho5 from "../src/assets/storybg.jpg";
 
 const Hero = lazy(() => import("./components/Hero"));
 const FeaturesOfHomePage = lazy(() => import("./components/Features"));
